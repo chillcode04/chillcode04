faf19c6533b8daacb934ff497b1134bac1a1c7a7
chillcode04 <nguyenhuy180904@gmail.com>
2024-05-22 19:29:40 +0700

    [ADD] Add intial UI for user

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
 int main () {
+    printf("\nSIMPLE CALCULATOR");
+
+    printf("\n1. Basic Calculation");
+    printf("\n2. GCD");
+    printf("\n3. Exit");
+
+    printf("\n Select option [1-3]: ");
     return 0;
 }
