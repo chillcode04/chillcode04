87f2676013afe2220fa0df91969af9d392adc037
chillcode04 <nguyenhuy180904@gmail.com>
2024-05-22 19:51:41 +0700

    [ADD] Add .gitognore and remove main.exe from git

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 int main () {
     printf("\nSIMPLE CALCULATOR");
 
@@ -7,5 +8,10 @@ int main () {
     printf("\n3. Exit");
 
     printf("\n Select option [1-3]: ");
+
+
+    uint8_t option;
+    scanf("%hhu", &option);
+    printf("\nMode seleted: %hhu", option);
     return 0;
 }
